a3861795a41c80dba2393f0a64a2610ee7b645e5
araijus <82232243+araijus@users.noreply.github.com>
2025-06-09 11:07:52 +0200

    Add files via upload

---
+++ utils/hautoencoder.py
@@ -0,0 +1,231 @@
+import tensorflow as tf
+from keras.layers import Cropping2D
+
+LATENT_DIM=2
+EPS = 1e-8
+
+def exp_map(x):
+    norm = tf.norm(x, axis=-1, keepdims=True)
+    return tf.tanh(norm) * x / (norm + EPS)
+
+def log_map(x):
+    norm = tf.norm(x, axis=-1, keepdims=True)
+    return tf.atanh(norm) * x / (norm + EPS)
+
+class HyperbolicSampling(tf.keras.layers.Layer):
+  def call(self, inputs):
+    """Generates a random sample and combines with the encoder output
+
+    Args:
+      inputs -- output tensor from the encoder
+
+    Returns:
+      `inputs` tensors combined with a random sample
+    """
+
+    # unpack the output of the encoder
+    mu, sigma = inputs
+
+    # get the size and dimensions of the batch
+    batch = tf.shape(mu)[0]
+    dim = tf.shape(mu)[1]
+
+    # generate a random tensor
+    epsilon = tf.keras.backend.random_normal(shape=(batch, dim))
+
+    # combine the inputs and noise
+    return exp_map(mu + tf.exp(0.5 * sigma) * epsilon)
+
+    #z = mu + tf.exp(0.5 * log_sigma) * epsilon
+    #return exp_map(z)
+
+class KLDLayer(tf.keras.layers.Layer):
+  def call(self, inputs):
+    """Computes the KLD loss and adds it to the model
+
+    Args:
+      inputs -- tensor containing (mu, sigma)
+
+    Returns:
+      kl_loss -- computed Kullback–Leibler Divergence loss
+    """
+
+    # unpack the inputs
+    mu, sigma = inputs
+
+    # compute the loss
+    kl_loss = 1 + sigma - tf.square(mu) - tf.math.exp(sigma)
+    kl_loss = tf.reduce_mean(kl_loss) * -0.5
+
+
+    # store the result
+    self.add_loss(kl_loss)
+
+    return kl_loss
+
+def encoder_layers(inputs, latent_dim):
+  """Defines the encoder's layers.
+  Args:
+    inputs -- batch from the dataset
+    latent_dim -- dimensionality of the latent space
+
+  Returns:
+    mu -- learned mean
+    sigma -- learned standard deviation
+    batch_2.shape -- shape of the features before flattening
+  """
+
+  # add the Conv2D layers followed by BatchNormalization
+  x = tf.keras.layers.Conv2D(filters=20, kernel_size=3, strides=(1,2), padding="same", activation='relu', name="encode_conv1")(inputs)
+  x = tf.keras.layers.BatchNormalization()(x)
+
+  x = tf.keras.layers.Conv2D(filters=40, kernel_size=3, strides=(1,2), padding='same', activation='relu', name="encode_conv2")(x)
+  x = tf.keras.layers.BatchNormalization()(x)
+
+  x = tf.keras.layers.Conv2D(filters=80, kernel_size=3, strides=(1,2), padding='same', activation='relu', name="encode_conv3")(x)
+  #x = tf.keras.layers.BatchNormalization()(x)
+
+  #shape_before_flattening = K.int_shape(x)
+  #x = layers.Flatten()(x)
+
+  # assign to a different variable so you can extract the shape later
+  batch_2 = tf.keras.layers.BatchNormalization()(x)
+
+  # flatten the features and feed into the Dense network
+  x = tf.keras.layers.Flatten(name="encode_flatten")(batch_2)
+
+  # we arbitrarily used 16 units here but feel free to change and see what results you get
+  x = tf.keras.layers.Dense(16, activation='relu', name="encode_dense")(x)
+  x = tf.keras.layers.BatchNormalization()(x)
+
+  # add output Dense networks for mu and sigma, units equal to the declared latent_dim.
+  mu = tf.keras.layers.Dense(latent_dim, name='latent_mu', activation=None)(x)
+  sigma = tf.keras.layers.Dense(latent_dim, name ='latent_sigma', activation=None)(x)
+
+  return mu, sigma, batch_2.shape
+
+def encoder_model(latent_dim, input_shape):
+  """Defines the encoder model with the Sampling layer
+  Args:
+    latent_dim -- dimensionality of the latent space
+    input_shape -- shape of the dataset batch
+
+  Returns:
+    model -- the encoder model
+    conv_shape -- shape of the features before flattening
+  """
+
+  # declare the inputs tensor with the given shape
+  inputs = tf.keras.Input(shape=input_shape, name='encoder_input')
+
+  # get the output of the encoder_layers() function
+  mu, sigma, conv_shape = encoder_layers(inputs, latent_dim=LATENT_DIM)
+
+  # feed mu and sigma to the Sampling layer
+  z = HyperbolicSampling()((mu, sigma))
+
+  # feed mu and sigma to the KLD layer
+  kl_loss = KLDLayer()((mu, sigma))
+
+  # build the whole encoder model
+  model = tf.keras.Model(inputs=inputs, outputs=[z, kl_loss])
+
+  return model, conv_shape
+
+def decoder_layers(inputs, conv_shape):
+  """Defines the decoder layers.
+  Args:
+    inputs -- output of the encoder
+    conv_shape -- shape of the features before flattening
+
+  Returns:
+    tensor containing the decoded output
+  """
+  z_input = tf.keras.Input(shape=(LATENT_DIM,), name='decoder_input')
+
+  # Map back from hyperbolic to Euclidean space
+  z_euclidean = tf.keras.layers.Lambda(log_map)(z_input)
+
+  # feed to a Dense network with units computed from the conv_shape dimensions
+  units = conv_shape[1] * conv_shape[2] * conv_shape[3]
+  x = tf.keras.layers.Dense(units, activation = 'relu', name="decode_dense1")(inputs)
+  #x = tf.keras.layers.BatchNormalization()(x)
+
+  # reshape output using the conv_shape dimensions
+  x = tf.keras.layers.Reshape((conv_shape[1], conv_shape[2], conv_shape[3]), name="decode_reshape")(x)
+
+  # upsample the features back to the original dimensions
+  x = tf.keras.layers.Conv2DTranspose(filters=80, kernel_size=3, strides=(1,2), padding='same', activation='relu', name="decode_conv2d_1")(x)
+  x = tf.keras.layers.BatchNormalization()(x)
+
+  x = tf.keras.layers.Conv2DTranspose(filters=40, kernel_size=3, strides=(1,2), padding='same', activation='relu', name="decode_conv2d_2")(x)
+  x = tf.keras.layers.BatchNormalization()(x)
+
+  x = tf.keras.layers.Conv2DTranspose(filters=20, kernel_size=3, strides=(1,2), padding='same', activation='relu', name="decode_conv2d_3")(x)
+  x = tf.keras.layers.BatchNormalization()(x)
+
+  return x
+
+def decoder_model(latent_dim, conv_shape):
+  """Defines the decoder model.
+  Args:
+    latent_dim -- dimensionality of the latent space
+    conv_shape -- shape of the features before flattening
+
+  Returns:
+    model -- the decoder model
+  """
+
+  # set the inputs to the shape of the latent space
+  inputs = tf.keras.Input(shape=(latent_dim,))
+
+  # get the output of the decoder layers
+  outputs = decoder_layers(inputs, conv_shape)
+
+  # declare the inputs and outputs of the model
+  model = tf.keras.Model(inputs, outputs)
+
+  return model
+
+
+def HyperbolicVAE(encoder, decoder, input_shape):
+  """Defines the VAE model
+  Args:
+    encoder -- the encoder model
+    decoder -- the decoder model
+    input_shape -- shape of the dataset batch
+
+  Returns:
+    the complete VAE model
+  """
+
+  # set the inputs
+  inputs = tf.keras.Input(shape=input_shape)
+
+  # get z from the encoder output
+  z, _ = encoder(inputs)
+
+  # get reconstructed output from the decoder
+  reconstructed = decoder(z)
+
+  # define the inputs and outputs of the VAE
+  model = tf.keras.Model(inputs=inputs, outputs=reconstructed)
+
+  return model
+
+def get_models(input_shape, latent_dim):
+  """Returns the encoder, decoder, and vae models"""
+  encoder, conv_shape = encoder_model(latent_dim=latent_dim, input_shape=input_shape)
+  decoder = decoder_model(latent_dim=latent_dim, conv_shape=conv_shape)
+  hvae = HyperbolicVAE(encoder, decoder, input_shape=input_shape)
+  return encoder, decoder, hvae
+
+# Get the encoder, decoder and 'master' model (called vae) #input_shape=(256,626,1)
+encoder, decoder, hvae = get_models(input_shape=(5,80,1), latent_dim=LATENT_DIM)
+
+hvae.summary()
+
+# Define the loss functions and optimizers
+optimizer = tf.keras.optimizers.Adam()
+loss_metric = tf.keras.metrics.Mean()
+mse_loss = tf.keras.losses.MeanSquaredError()
